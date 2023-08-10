4e3e8285190193cb3df812f3c47413eb74389f6a
tatasaiharsha <31988217+tatasaiharsha@users.noreply.github.com>
2023-08-09 22:06:34 -0500

    Add project files.

---
+++ VBwebform/Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Title="Home Page" Language="VB" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.vb" Inherits="VBwebform._Default" %>
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+
+ <div style ="background-color:darkgoldenrod; font-size:x-large; color:white; align-content:center">
+     CRUD operations in ASP VB.NET
+
+ </div>
+    <br />
+    <div style="padding:20px"> Various Products Information
+
+        <table class="nav-justified">
+            <tr>
+                <td>ProductID</td>
+                <td>
+                    <asp:TextBox ID="txtproductID" runat="server" Font-Size="Medium" Width="243px"></asp:TextBox>
+                &nbsp;
+                    <asp:Button ID="Button5" runat="server" Font-Size="Small" ForeColor="Black" Height="24px" Text="Search" Width="76px" />
+                </td>
+            </tr>
+            <tr>
+                <td>Item name</td>
+                <td>
+                    <asp:TextBox ID="txtitemname" runat="server" Font-Size="Medium" Width="243px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Specification</td>
+                <td>
+                    <asp:TextBox ID="txtspecification" runat="server" Font-Size="Medium" Width="243px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Unit</td>
+                <td>
+                    <asp:DropDownList ID="Dropunit" runat="server" Font-Size="Medium" Width="200px">
+                        <asp:ListItem>KG</asp:ListItem>
+                        <asp:ListItem>PCS</asp:ListItem>
+                        <asp:ListItem>DZ</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+            </tr>
+            <tr>
+                <td style="height: 20px">Color</td>
+                <td style="height: 20px">
+                    <asp:RadioButtonList ID="Radiocolor" runat="server" Font-Size="Medium" RepeatDirection="Horizontal" Width="268px">
+                        <asp:ListItem>Grey</asp:ListItem>
+                        <asp:ListItem>White</asp:ListItem>
+                        <asp:ListItem>Black</asp:ListItem>
+                    </asp:RadioButtonList>
+                </td>
+            </tr>
+            <tr>
+                <td>Insert date</td>
+                <td>
+                    <asp:TextBox ID="txtdate" runat="server" Font-Size="Medium" Width="243px"></asp:TextBox>
+  
+                    <ajaxToolkit:CalendarExtender ID="CalendarExtender" runat="server" TargetControlID="txtdate">
+</ajaxToolkit:CalendarExtender>
+                </td>
+            </tr>
+            <tr>
+                <td>Opening Quantity</td>
+                <td>
+                    <br />
+                    <asp:TextBox ID="txtopeningqty" runat="server" Font-Size="Medium" Width="243px"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>Product Status</td>
+                <td>
+                    <asp:CheckBox ID="Checkregular" runat="server" Text="Regular" />
+&nbsp;<asp:CheckBox ID="Checkirregular" runat="server" Text="Irregular" />
+                </td>
+            </tr>
+            <tr>
+                <td>&nbsp;</td>
+                <td id="button3">
+                    <asp:Button ID="Button2" runat="server" Font-Size="Medium" ForeColor="Black" Height="41px" Text="Insert" Width="102px" />
+                &nbsp;
+                    <asp:Button ID="Button3" runat="server" Font-Size="Medium" ForeColor="Black" Height="41px" Text="Update" Width="102px" />
+                &nbsp;<asp:Button ID="Button4" runat="server" Font-Size="Medium" ForeColor="Black" Height="41px" Text="Delete" Width="102px" OnClientClick="return confirm('Are you sure to delete?')" />
+                </td>
+            </tr>
+        </table>
+        <div align="center">
+            <hr />
+
+            <asp:GridView ID="GridView1" runat="server" Width="600px">
+                <HeaderStyle BackColor="darkgoldenrod" ForeColor="White" />
+            </asp:GridView>
+
+        </div>
+    </div>
+
+</asp:Content>
---
+++ VBwebform/Default.aspx.vb
@@ -0,0 +1,116 @@
+﻿Imports System.Data.SqlClient
+Imports System.Web.UI.HtmlControls
+
+
+Public Class _Default
+    Inherits Page
+
+    Protected Sub Button1_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim connect As New SqlConnection("Data Source=HARSHA-YOGA;Initial Catalog=CRUD_DB;User ID=sa;Password=******")
+        Dim productid As Integer = txtproductID.Text
+        Dim itemname As String = txtitemname.Text
+        Dim specification As String = txtspecification.Text
+        Dim unit As String = Dropunit.SelectedValue
+        Dim color As String = Radiocolor.SelectedValue
+        Dim insertdate As DateTime = txtdate.Text
+        Dim opening As Double = txtopeningqty.Text
+        Dim status As String = ""
+        If Checkregular.Checked = True Then
+            status = "Regular"
+        Else
+            status = "irregular"
+
+        End If
+        connect.Open()
+
+        Dim command As New SqlCommand("Insert into ProductInfo_Tab values('" & productid & "', '" & itemname & "','" & specification & "','" & unit & "','" & color & "','" & insertdate & "','" & opening & "','" & status & "')", connect
+                                      )
+        command.ExecuteNonQuery()
+        MsgBox("Successfully inserted", MsgBoxStyle.Information, "MEssage")
+        connect.Close()
+
+        ListProduct()
+
+
+    End Sub
+
+    Protected Sub ListProduct()
+        Dim connect As New SqlConnection("Data Source=HARSHA-YOGA;Initial Catalog=CRUD_DB;User ID=sa;Password=******")
+        Dim command As New SqlCommand("select * from ProductInfo_Tab", connect)
+        Dim sd As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sd.Fill(dt)
+        GridView1.DataSource = dt
+        GridView1.DataBind()
+
+
+    End Sub
+
+    Protected Sub TextBox1_TextChanged(sender As Object, e As EventArgs)
+
+    End Sub
+
+    Protected Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim connect As New SqlConnection("Data Source=HARSHA-YOGA;Initial Catalog=CRUD_DB;User ID=sa;Password=******")
+        Dim productid As Integer = txtproductID.Text
+        Dim itemname As String = txtitemname.Text
+        Dim specification As String = txtspecification.Text
+        Dim unit As String = Dropunit.SelectedValue
+        Dim color As String = Radiocolor.SelectedValue
+        Dim insertdate As DateTime = txtdate.Text
+        Dim opening As Double = txtopeningqty.Text
+        Dim status As String = ""
+        If Checkregular.Checked = True Then
+            status = "Regular"
+        Else
+            status = "irregular"
+
+        End If
+        connect.Open()
+
+        Dim command As New SqlCommand("update ProductInfo_Tab set ItemName = '" & itemname & "', Specification = '" & specification & "', Unit = '" & unit & "', Color  = '" & color & "', InsertDate = '" & insertdate & "', Opening = '" & opening & "', Status = '" & status & "' where ProductID = '" & productid & "'", connect)
+
+        command.ExecuteNonQuery()
+        MsgBox("Successfully updated", MsgBoxStyle.Information, "Message")
+        connect.Close()
+
+        ListProduct()
+
+
+    End Sub
+
+    Protected Sub txtdate_TextChanged(sender As Object, e As EventArgs) Handles txtdate.TextChanged
+
+    End Sub
+
+    Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ListProduct()
+    End Sub
+
+    Protected Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Dim connect As New SqlConnection("Data Source=HARSHA-YOGA;Initial Catalog=CRUD_DB;User ID=sa;Password=********")
+        Dim productid As Integer = txtproductID.Text
+        connect.Open()
+
+        Dim command As New SqlCommand("Delete ProductInfo_Tab where ProductID = '" & productid & "'", connect)
+
+        command.ExecuteNonQuery()
+        MsgBox("Successfully deleted", MsgBoxStyle.Information, "Message")
+        connect.Close()
+
+        ListProduct()
+
+    End Sub
+
+    Protected Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim productid As Integer = txtproductID.Text
+        Dim connect As New SqlConnection("Data Source=HARSHA-YOGA;Initial Catalog=CRUD_DB;User ID=sa;Password=*******")
+        Dim command As New SqlCommand("select * from ProductInfo_Tab where ProductID = '" & productid & "' ", connect)
+        Dim sd As New SqlDataAdapter(command)
+        Dim dt As New DataTable
+        sd.Fill(dt)
+        GridView1.DataSource = dt
+        GridView1.DataBind()
+
+    End Sub
+End Class
